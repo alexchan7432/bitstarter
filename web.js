91f11735754725340074198a8a4e96217d8e7840
Ubuntu <ubuntu@ip-172-31-22-17.us-west-2.compute.internal>
2013-07-04 13:39:41 +0000

    added index.html buffer

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var buffer = new Buffer(fs.readFileSync('index.html'));
-
-    response.send('Hello World2!');
+    
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
